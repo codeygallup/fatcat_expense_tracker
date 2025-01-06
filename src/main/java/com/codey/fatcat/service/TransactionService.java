06d33912aa657023998a9e9c4a6db723af305444
Codey Gallup <codey.gallup@gmail.com>
2025-01-05 19:41:50 -0700

    Prepping balance changes

--- src/main/java/com/codey/fatcat/service/TransactionService.java
+++ src/main/java/com/codey/fatcat/service/TransactionService.java
@@ -1,4 +1,16 @@
 package com.codey.fatcat.service;
 
 public class TransactionService {
+//  public void addTransaction(Transaction transaction) {
+//    Account account = transaction.getAccount();
+//
+//    if ("credit".equalsIgnoreCase(transaction.getTransactionType())) {
+//      account.setBalance(account.getBalance() + transaction.getAmount());
+//    } else if ("debit".equalsIgnoreCase(transaction.getTransactionType())) {
+//      account.setBalance(account.getBalance() - transaction.getAmount());
+//    }
+//
+//    accountRepository.save(account);
+//    transactionRepository.save(transaction);
+//  }
 }
