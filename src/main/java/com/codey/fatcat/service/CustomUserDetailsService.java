f5734cd495d779057d5116f6dfb173ebd1db38e8
Codey Gallup <codey.gallup@gmail.com>
2025-01-06 08:38:33 -0700

    Starting UserDetails and security

---
+++ src/main/java/com/codey/fatcat/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.codey.fatcat.service;
+
+import com.codey.fatcat.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public CustomUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    com.codey.fatcat.entity.User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+    return org.springframework.security.core.userdetails.User.builder()
+        .username(user.getName())
+        .password(user.getPassword())
+        .authorities(
+            String.valueOf(user.getRole()))
+        .build();
+
+  }
+}
