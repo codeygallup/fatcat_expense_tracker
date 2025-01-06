02d572e47a9b92721312e608ec9145bc4796c1c2
Codey Gallup <codey.gallup@gmail.com>
2025-01-06 08:01:55 -0700

    Setting up repositories and bcrypt

--- src/main/java/com/codey/fatcat/repository/TransactionRepository.java
+++ src/main/java/com/codey/fatcat/repository/TransactionRepository.java
@@ -1,4 +1,9 @@
 package com.codey.fatcat.repository;
 
-public class TransactionRepository {
+import com.codey.fatcat.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 }
