c03b15e590d3dc7c3ce606a61bef9efbce387db5
Codey Gallup <codey.gallup@gmail.com>
2025-01-06 15:03:01 -0700

    Able to register new users

--- src/main/java/com/codey/fatcat/config/SecurityConfig.java
+++ src/main/java/com/codey/fatcat/config/SecurityConfig.java
@@ -1,23 +1,69 @@
 package com.codey.fatcat.config;
 
+import com.codey.fatcat.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+  private final CustomUserDetailsService customUserDetailsService;
+
+  public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    this.customUserDetailsService = customUserDetailsService;
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth
-        .userDetailsService(userDetailsService)
-        .passwordEncoder(passwordEncoder());
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .userDetailsService(customUserDetailsService)
+        .authorizeHttpRequests((authorize) -> authorize
+            .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
+            .anyRequest().authenticated()
+        )
+        .csrf(AbstractHttpConfigurer::disable)
+        .httpBasic(Customizer.withDefaults())
+        .build();
   }
 }
+
+
+//package com.codey.fatcat.config;
+//
+//import com.codey.fatcat.service.CustomUserDetailsService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    return http
+//        .userDetailsService(customUserDetailsService)
+//        .authorizeHttpRequests((authorize) -> authorize
+//            .anyRequest().permitAll() // Allow all requests
+//        )
+//        .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
+//        .httpBasic(Customizer.withDefaults())
+//        .build();
+//  }
+//}
