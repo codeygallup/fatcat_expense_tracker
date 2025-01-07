a9933a56bda2bb57ae1e28e8d96e3b219c6f0b0c
Codey Gallup <codey.gallup@gmail.com>
2025-01-07 08:41:27 -0700

    Moving logic over to service layer

--- src/main/java/com/codey/fatcat/config/SecurityConfig.java
+++ src/main/java/com/codey/fatcat/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-        .userDetailsService(customUserDetailsService)
+//        .userDetailsService(customUserDetailsService)
         .authorizeHttpRequests((authorize) -> authorize
             .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
             .anyRequest().authenticated()
@@ -40,30 +40,3 @@ public class SecurityConfig {
         .build();
   }
 }
-
-
-//package com.codey.fatcat.config;
-//
-//import com.codey.fatcat.service.CustomUserDetailsService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//  @Bean
-//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    return http
-//        .userDetailsService(customUserDetailsService)
-//        .authorizeHttpRequests((authorize) -> authorize
-//            .anyRequest().permitAll() // Allow all requests
-//        )
-//        .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
-//        .httpBasic(Customizer.withDefaults())
-//        .build();
-//  }
-//}
--- src/main/java/com/codey/fatcat/service/UserService.java
+++ src/main/java/com/codey/fatcat/service/UserService.java
@@ -1,20 +1,26 @@
 package com.codey.fatcat.service;
 
 import com.codey.fatcat.entity.User;
+import com.codey.fatcat.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+  private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
 
-  public UserService(PasswordEncoder passwordEncoder) {
+  @Autowired
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
     this.passwordEncoder = passwordEncoder;
   }
 
-  public void saveUser(User user) {
+  public void registerUser(User user) {
     user.setPassword(passwordEncoder.encode(user.getPassword()));
+    userRepository.save(user);
   }
 
 }
