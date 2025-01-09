e335f0b4afa2d450080981f4ddd62928c332d23e
Codey Gallup <codey.gallup@gmail.com>
2025-01-09 12:33:37 -0700

    CRUD functions for account table

--- src/main/java/com/codey/fatcat/controller/AccountController.java
+++ src/main/java/com/codey/fatcat/controller/AccountController.java
@@ -1,4 +1,61 @@
 package com.codey.fatcat.controller;
 
+import com.codey.fatcat.dto.AccountDTO;
+import com.codey.fatcat.entity.Account;
+import com.codey.fatcat.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/accounts")
 public class AccountController {
+
+  private final AccountService accountService;
+
+  public AccountController(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Account>> getAllAccounts() {
+    return ResponseEntity.ok(accountService.getAllAccounts());
+//    List<Account> accounts = accountService.getAllAccounts();
+//    List<AccountDTO> accountDTOS = accounts.stream()
+//        .map(account -> new AccountDTO(account.getId(), account.getAccountType(),
+//                                       account.getBalance(),
+//                                       account.getUser().getId()))
+//        .toList();
+//    return new ResponseEntity<>(accountDTOS, HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Account> getAccountById(@PathVariable UUID id) {
+//    Account account = accountService.getAccountById(id);
+//    return new ResponseEntity<>(new AccountDTO(account.getId(), account.getAccountType(),
+//                                               account.getBalance(),
+//                                               account.getUser().getId()), HttpStatus.OK);
+    return new ResponseEntity<>(accountService.getAccountById(id), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<Account> createAccount(@RequestBody AccountDTO account) {
+    return new ResponseEntity<>(accountService.createAccount(account), HttpStatus.CREATED);
+  }
+
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteAccount(@PathVariable UUID id) {
+    return accountService.deleteAccount(id) ? ResponseEntity.ok("Account deleted successfully") :
+        ResponseEntity.notFound().build();
+  }
 }
--- src/main/java/com/codey/fatcat/service/AccountService.java
+++ src/main/java/com/codey/fatcat/service/AccountService.java
@@ -1,4 +1,60 @@
 package com.codey.fatcat.service;
 
+import com.codey.fatcat.dto.AccountDTO;
+import com.codey.fatcat.entity.Account;
+import com.codey.fatcat.entity.User;
+import com.codey.fatcat.repository.AccountRepository;
+import com.codey.fatcat.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class AccountService {
+
+  private final AccountRepository accountRepository;
+  private final UserRepository userRepository;
+
+  public AccountService(AccountRepository accountRepository, UserRepository userRepository) {
+    this.accountRepository = accountRepository;
+    this.userRepository = userRepository;
+  }
+
+  public List<Account> getAllAccounts() {
+    return accountRepository.findAll();
+  }
+
+  public Account getAccountById(UUID id) {
+    return accountRepository.findById(id).orElse(null);
+  }
+
+  public Account createAccount(AccountDTO account) {
+    User user = userRepository.findById(account.getUserId())
+        .orElseThrow(() -> new RuntimeException("User not found"));
+//    UUID userId = user.getId();
+    Account newAccount = new Account();
+    newAccount.setAccountType(account.getAccountType());
+    newAccount.setBalance(account.getBalance());
+    newAccount.setUser(user);
+    return accountRepository.save(newAccount);
+  }
+
+  @Transactional
+  public boolean deleteAccount(UUID id) {
+    if (accountRepository.existsById(id)) {
+      Account account = accountRepository.findById(id).orElse(null);
+      if (account != null) {
+        account.getUser().getAccounts().remove(account);
+        accountRepository.deleteById(id);
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
+
+
+
