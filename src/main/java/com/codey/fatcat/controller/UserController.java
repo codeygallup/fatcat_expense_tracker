94d6ab502c0dce6769fbb083caf79da6ef0d084b
Codey Gallup <codey.gallup@gmail.com>
2025-01-09 08:22:25 -0700

    Get all and single user, created DTO

--- src/main/java/com/codey/fatcat/controller/UserController.java
+++ src/main/java/com/codey/fatcat/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.codey.fatcat.controller;
 
+import com.codey.fatcat.dto.UserDTO;
 import com.codey.fatcat.entity.User;
 import com.codey.fatcat.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -22,11 +29,43 @@ public class UserController {
   public ResponseEntity<String> registerUser(@RequestBody User user) {
     userService.registerUser(user);
     return ResponseEntity.ok("User registered successfully");
+//    return new ResponseEntity<>(userService.registerUser(user), HttpStatus.CREATED);
   }
 
   @PostMapping("/login")
   public ResponseEntity<String> loginUser(@RequestBody User user) {
     userService.loginUser(user);
     return ResponseEntity.ok("User logged in successfully");
+  }
+
+  @GetMapping
+  public ResponseEntity<List<UserDTO>> getAllUsers() {
+//    return ResponseEntity.ok(userService.getAllUsers());
+    List<User> users = userService.getAllUsers();
+    List<UserDTO> userDTOs = users.stream()
+        .map(user -> new UserDTO(user.getId(), user.getName(), user.getEmail()))
+        .toList();
+    return ResponseEntity.ok(userDTOs);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<UserDTO> getUserById(@PathVariable UUID id) {
+    System.out.println("CONTROLLERID: " + id);
+    User user = userService.getUserById(id);
+    System.out.println("CONTROLLERUSER: " + user);
+    return ResponseEntity.ok(new UserDTO(user.getId(), user.getName(), user.getEmail()));
+  }
+
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<String> deleteUser(@PathVariable UUID id) {
+    System.out.println("CONTROLLERID" + id.getClass());
+//    UUID uuidId = UUID.fromString
+//    System.out.println("CONTROLLERUUID: " + uuidId);
+////    System.out.println("CONTROLLERID CLASS: " + uuidId.getClass());
+//    userService.deleteUser(id);
+    return null;
+//    return userService.deleteUser(id) ? ResponseEntity.ok("User deleted successfully") :
+//        ResponseEntity.notFound().build();
   }
 }
