d19c5004ecfc7c65a5b3727beb902e7cd8e346f5
Codey Gallup <codey.gallup@gmail.com>
2025-01-09 14:26:16 -0700

    Adding accounts to UserDTO

--- src/main/java/com/codey/fatcat/dto/UserDTO.java
+++ src/main/java/com/codey/fatcat/dto/UserDTO.java
@@ -1,9 +1,11 @@
 package com.codey.fatcat.dto;
 
+import com.codey.fatcat.entity.Account;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Getter
@@ -14,5 +16,6 @@ public class UserDTO {
   private UUID id;
   private String name;
   private String email;
+  private Set<Account> accounts;
 
 }
