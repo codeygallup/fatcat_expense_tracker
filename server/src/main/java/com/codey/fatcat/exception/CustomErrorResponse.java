06cdad070c7e3dbec799432b4d428d538d036786
Codey Gallup <codey.gallup@gmail.com>
2025-01-11 16:19:05 -0700

    New exceptions

--- server/src/main/java/com/codey/fatcat/exception/CustomErrorResponse.java
+++ server/src/main/java/com/codey/fatcat/exception/CustomErrorResponse.java
@@ -1,5 +1,6 @@
 package com.codey.fatcat.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CustomErrorResponse {
 
   private int status;
   private String message;
+  private String error;
   private LocalDateTime timestamp;
+  private String path;
+
 }
