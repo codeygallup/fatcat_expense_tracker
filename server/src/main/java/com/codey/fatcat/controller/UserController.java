ce0e789a9f9ef8317e54d944bcf5bf52b734409e
Codey Gallup <codey.gallup@gmail.com>
2025-01-11 13:20:48 -0700

    Moving authentication

--- server/src/main/java/com/codey/fatcat/controller/UserController.java
+++ server/src/main/java/com/codey/fatcat/controller/UserController.java
@@ -2,9 +2,15 @@ package com.codey.fatcat.controller;
 
 import com.codey.fatcat.dto.UserDTO;
 import com.codey.fatcat.entity.User;
+import com.codey.fatcat.service.CustomUserDetailsService;
 import com.codey.fatcat.service.UserService;
+import com.codey.fatcat.webtoken.JwtService;
+import com.codey.fatcat.webtoken.LoginForm;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +28,16 @@ import java.util.UUID;
 public class UserController {
 
   private final UserService userService;
+  private final AuthenticationManager authenticationManager;
+  private final JwtService jwtService;
+  private final CustomUserDetailsService customUserDetailsService;
 
-  public UserController(UserService userService) {
+  public UserController(UserService userService, AuthenticationManager authenticationManager, JwtService jwtService,
+                        CustomUserDetailsService customUserDetailsService) {
     this.userService = userService;
+    this.authenticationManager = authenticationManager;
+    this.jwtService = jwtService;
+    this.customUserDetailsService = customUserDetailsService;
   }
 
   @PostMapping("/register")
@@ -35,9 +48,16 @@ public class UserController {
   }
 
   @PostMapping("/login")
-  public ResponseEntity<String> loginUser(@RequestBody User user) {
-    userService.loginUser(user);
-    return ResponseEntity.ok("User logged in successfully");
+  public ResponseEntity<String> loginUser(@RequestBody LoginForm loginForm) {
+    Authentication authentication =
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginForm.email(),
+                                                                                   loginForm.password()));
+
+    if (authentication.isAuthenticated()) {
+      String token = jwtService.generateToken(customUserDetailsService.loadUserByUsername(loginForm.email()));
+      return ResponseEntity.ok(token);
+    }
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
   }
 
   @GetMapping
