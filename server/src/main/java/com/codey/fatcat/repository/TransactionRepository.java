7303b1b70983783903e5df554dbb4b49296d11b7
Codey Gallup <codey.gallup@gmail.com>
2025-01-12 17:18:53 -0700

    New custom methods

--- server/src/main/java/com/codey/fatcat/repository/TransactionRepository.java
+++ server/src/main/java/com/codey/fatcat/repository/TransactionRepository.java
@@ -3,7 +3,9 @@ package com.codey.fatcat.repository;
 import com.codey.fatcat.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+  List<Transaction> findAllByAccount_User_Email(String email);
 }
