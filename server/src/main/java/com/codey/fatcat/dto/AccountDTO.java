fd4e0e215b01c4c5470094987298c212e4b28c56
Codeyg12 <codey.gallup@gmail.com>
2025-07-13 16:08:12 -0600

    Improving entities and dtos

--- server/src/main/java/com/codey/fatcat/dto/AccountDTO.java
+++ server/src/main/java/com/codey/fatcat/dto/AccountDTO.java
@@ -2,6 +2,7 @@ package com.codey.fatcat.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AccountDTO {
 
   private UUID id;
