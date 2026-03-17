c2b60ce4bfe3b54a78f65bb56d9ba410a4c24178
Codeyg12 <codey.gallup@gmail.com>
2026-03-16 20:05:28 -0600

    Starter controller/service for bill

---
+++ server/src/main/java/com/codey/fatcat/controller/BillController.java
@@ -0,0 +1,55 @@
+package com.codey.fatcat.controller;
+
+import com.codey.fatcat.dto.BillDTO;
+import com.codey.fatcat.entity.Bill;
+import com.codey.fatcat.service.BillService;
+import com.codey.fatcat.utils.DTOConverter;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/bills")
+public class BillController {
+
+    private final BillService billService;
+
+    public BillController(BillService billService) {
+        this.billService = billService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BillDTO>> getAllBills() {
+        List<Bill> bills = billService.getAllBills();
+        List<BillDTO> billDTOs = bills.stream().map(DTOConverter::convertToDTO).toList();
+        return ResponseEntity.ok(billDTOs);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BillDTO> getBillById(@PathVariable UUID id) {
+        Bill bill = billService.getBillById(id);
+        return ResponseEntity.ok(DTOConverter.convertToDTO(bill));
+    }
+
+    @PostMapping
+    public ResponseEntity<BillDTO> createBill(@Valid @RequestBody BillDTO billDTO) {
+        Bill bill = billService.createBill(billDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(DTOConverter.convertToDTO(bill));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BillDTO> updateBill(@PathVariable UUID id, @Valid @RequestBody BillDTO billDTO) {
+        Bill bill = billService.updateBill(id, billDTO);
+        return ResponseEntity.accepted().body(DTOConverter.convertToDTO(bill));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<BillDTO> deleteBill(@PathVariable UUID id) {
+        return billService.deleteBill(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
+
+}
