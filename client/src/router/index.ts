ec07722fc3002adcb7ae386006f84ab79e8cf82b
Codey Gallup <codey.gallup@gmail.com>
2026-03-22 19:08:35 -0600

    Initial FE (#2)
    * Removed Next replaced with Vue
    
    * Adding gitignore in root
    
    * Fixing name & adding routes
    
    * Starter Navbar
    
    * Starter files for routes
    
    * Fixing styling to account for navbar, replacing a tags with RouterLinks
    
    * Starting login pages
    
    * Accounts page
    
    * Bill page
    
    * Spending page
    
    * Fixing CORS
    
    * Creating auth
    
    * Adding auth context
    
    * AuthForm updated for context
    
    * Fixing vue console error, adding new routes
    
    * Touching up pages
    
    * Dashboard with links to all base pages
    
    * Adding ApexCharts
    
    * Adding optional headers

---
+++ client/src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'Home', component: () => import('@/views/Home.vue') },
+    { path: '/login', name: 'Login', component: () => import('@/views/Login.vue') },
+    { path: '/register', name: 'Register', component: () => import('@/views/Register.vue') },
+    { path: '/dashboard', name: 'Dashboard', component: () => import('@/views/Dashboard.vue') },
+    { path: '/accounts', name: 'Accounts', component: () => import('@/views/Account.vue') },
+    { path: '/bills', name: 'Bills', component: () => import('@/views/Bill.vue') },
+    { path: '/transactions', name: 'Transactions', component: () => import('@/views/Spending.vue') }
+  ],
+})
+
+router.beforeEach((to) => {
+  const token = localStorage.getItem('token')
+  const publicPages = ['Home', 'Login', 'Register']
+
+  if (!token && !publicPages.includes(to.name as string)) {
+    return '/'
+  }
+})
+
+export default router
