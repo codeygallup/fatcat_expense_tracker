afcd79f908c1ce9c47a078259227f19ab2c35e37
Codeyg12 <codey.gallup@gmail.com>
2026-03-22 20:11:05 -0600

    recent transactions api

--- server/src/main/java/com/codey/fatcat/controller/TransactionController.java
+++ server/src/main/java/com/codey/fatcat/controller/TransactionController.java
@@ -5,6 +5,7 @@ import com.codey.fatcat.entity.Transaction;
 import com.codey.fatcat.service.TransactionService;
 import com.codey.fatcat.utils.DTOConverter;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -40,6 +41,13 @@ public class TransactionController {
     public ResponseEntity<TransactionDTO> getTransactionById(@PathVariable UUID id) {
         Transaction transaction = transactionService.getTransactionById(id);
         return ResponseEntity.ok(DTOConverter.convertToDTO(transaction));
+    }
+
+    @GetMapping("/recent")
+    public ResponseEntity<List<TransactionDTO>> getRecentTransactions() {
+        Page<Transaction> transactions = transactionService.getRecentTransactions();
+        List<TransactionDTO> transactionDTOs = transactions.stream().map(DTOConverter::convertToDTO).toList();
+        return ResponseEntity.ok(transactionDTOs);
     }
 
     @PostMapping
--- server/src/main/java/com/codey/fatcat/repository/TransactionRepository.java
+++ server/src/main/java/com/codey/fatcat/repository/TransactionRepository.java
@@ -1,13 +1,18 @@
 package com.codey.fatcat.repository;
 
 import com.codey.fatcat.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
   List<Transaction> findAllByAccount_User_Email(String email);
+
+  Page<Transaction> findAllByAccount_UserId(UUID userId, Pageable pageable);
 }
