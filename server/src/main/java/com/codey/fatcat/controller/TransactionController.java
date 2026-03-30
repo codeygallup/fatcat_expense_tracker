91550bcb0dc5646854f1aebc8d85fdc66fc8a9d2
Codeyg12 <codey.gallup@gmail.com>
2026-03-29 20:07:25 -0600

    Adding fetch transactions by account

--- server/src/main/java/com/codey/fatcat/controller/TransactionController.java
+++ server/src/main/java/com/codey/fatcat/controller/TransactionController.java
@@ -8,14 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -31,8 +24,10 @@ public class TransactionController {
     }
 
     @GetMapping
-    public ResponseEntity<List<TransactionDTO>> getAllTransactions() {
-        List<Transaction> transactions = transactionService.getAllTransactions();
+    public ResponseEntity<List<TransactionDTO>> getAllTransactions(@RequestParam(required = false) UUID accountId) {
+        List<Transaction> transactions = accountId != null
+                ? transactionService.getTransactionsByAccount(accountId)
+                : transactionService.getAllTransactions();
         return ResponseEntity.ok(DTOConverter.toList(transactions, DTOConverter::convertToDTO));
     }
 
