b455adbea774f6011c13d5369892fffd3f564791
Codeyg12 <codey.gallup@gmail.com>
2026-04-14 17:49:22 -0600

    Adding a toast/confirmation modal

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import ToastContainer from './components/ToastContainer.vue';
 </script>
 
 <template>
   <div class="bg-gray-100 min-h-screen flex flex-col">
+    <ToastContainer />
     <Navbar />
     <main class="flex flex-1 flex-col">
       <RouterView />
