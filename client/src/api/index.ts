8db7777cd0a502848f2f17a0f242e94191ed6687
Codeyg12 <codey.gallup@gmail.com>
2026-04-18 20:11:01 -0600

    Moving to mysql and making variables to deploy

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api'
+const BASE_URL = import.meta.env.VITE_API_URL
 
 export const api = async (path: string, options?: RequestInit & { raw?: boolean }) => {
   const token = localStorage.getItem('token')
