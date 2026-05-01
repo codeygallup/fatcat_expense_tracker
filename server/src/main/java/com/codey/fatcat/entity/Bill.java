0ecaadefc79f4c84388b9ebc428b45d701ef51e3
Codeyg12 <codey.gallup@gmail.com>
2026-04-30 20:21:00 -0600

    Updating annotations on entities

--- server/src/main/java/com/codey/fatcat/entity/Bill.java
+++ server/src/main/java/com/codey/fatcat/entity/Bill.java
@@ -4,6 +4,8 @@ import com.codey.fatcat.enums.BillStatus;
 import com.codey.fatcat.enums.RecurringFrequency;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -18,29 +20,35 @@ import java.time.LocalDate;
 @Table(name = "bills")
 public class Bill extends BaseEntity {
 
+    @ToString.Exclude
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @ToString.Exclude
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "account_id")
     private Account account;
 
+    @NotBlank
     @Column(nullable = false)
     private String name;
 
     private String description;
 
+    @NotNull
     @Column(nullable = false)
     private BigDecimal amount;
 
+    @NotNull
     @Column(nullable = false)
     private LocalDate dueDate;
 
     private LocalDate lastPaidDate;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private BillStatus status;
